32f68d35f26af3b5fd3159e4d77ba96fa252903e
theVincenator <vincent.bruenjes@rwth-aachen.de>
2022-12-06 12:07:43 +0100

    solves day 02 again (rust practice)

--- d02/src/main.rs
+++ d02/src/main.rs
@@ -2,12 +2,47 @@ use std::fs;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Could not read file");
-
+    let mut ez_score = 0;
+    let mut normal_score = 0;
     for line in input.lines() {
-        if !line.is_empty() {
-            println!("{line}");
-        } else {
-            println!("<emptyline>");
-        }
+        let round = (
+            line.chars().next().expect("string was empty"),
+            line.chars().last().expect("string was empty"),
+        );
+        ez_score += calc_ez_score(round);
+        normal_score += calc_normal_score(round);
+    }
+
+    println!("Score 1: {ez_score}");
+    println!("Score 2: {normal_score}");
+}
+
+fn calc_ez_score(round: (char, char)) -> usize {
+    match round {
+        ('A', 'X') => 3 + 1,
+        ('A', 'Y') => 6 + 2,
+        ('A', 'Z') => 0 + 3,
+        ('B', 'X') => 0 + 1,
+        ('B', 'Y') => 3 + 2,
+        ('B', 'Z') => 6 + 3,
+        ('C', 'X') => 6 + 1,
+        ('C', 'Y') => 0 + 2,
+        ('C', 'Z') => 3 + 3,
+        _ => panic!(),
+    }
+}
+
+fn calc_normal_score(round: (char, char)) -> usize {
+    match round {
+        ('A', 'X') => 0 + 3,
+        ('A', 'Y') => 3 + 1,
+        ('A', 'Z') => 6 + 2,
+        ('B', 'X') => 0 + 1,
+        ('B', 'Y') => 3 + 2,
+        ('B', 'Z') => 6 + 3,
+        ('C', 'X') => 0 + 2,
+        ('C', 'Y') => 3 + 3,
+        ('C', 'Z') => 6 + 1,
+        _ => panic!(),
     }
 }
